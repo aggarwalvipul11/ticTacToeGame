703f486d1e8273b337f5258bfa7701de46925263
aggarwalvipul11 <vipul_dev@DESKTOP-QA3S37M.localdomain>
2020-07-16 09:52:55 +0530

    [VipulA] ADD: Choose letter X OR letter O

--- ticTacToeGame.sh
+++ ticTacToeGame.sh
@@ -22,14 +22,55 @@ function resetPlayerBoard() {
 
 #Player would begins with toss check who plays first
 function playerToss() {
-	if [[ $((RANDOM%2)) -eq 1 ]]
+	tossCheck=$((RANDOM%2))
+	if [[ $tossCheck -eq 1 ]]
 	then
 		echo "Player wins toss, start the game."
 	else
-		echo "Computer wins toss, start the game."
+		echo "Robot wins toss, start the game."
+	fi
+}
+
+function selectionForLXandLO() {
+	playerToss
+	if [[ $tossCheck -eq 1 ]]
+	then
+		echo "Please choose game X OR game O"
+		read symbolSelection
+		playerChanceChoose
+	else
+		echo "Robot wins, and choose the game sign"
+		computerChanceChoose
+	fi 
+	
+	#Display the symbols of game
+	echo "Player gets $playerGameIcon"
+	echo "Computer gets $computerGameIcon"
+}
+
+function playerChanceChoose() {
+	if [[ $symbolSelection -eq 'X' ]]
+	then
+		playerGameIcon=X;
+		computerGameIcon=O;
+	else
+		playerGameIcon=O;
+		computerGameIcon=X;
+	fi
+}
+
+function computerChanceChoose() {
+	if [[ $((RANDOM%2)) -eq 1 ]]
+	then
+		computerGameIcon=X;
+		playerGameIcon=O;
+	else
+		computerGameIcon=O;
+		playerGameIcon=X;
 	fi
 }
 
 resetPlayerBoard
 playerToss
+selectionForLXandLO
 #End of Use Case 02
