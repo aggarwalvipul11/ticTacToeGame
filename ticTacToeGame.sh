8753b7105ec30313362cce0bcd60c8f052711e3e
aggarwalvipul11 <vipul_dev@DESKTOP-QA3S37M.localdomain>
2020-07-16 10:39:42 +0530

    [VipulA] ADD: Player and computer displayed board and choose vaild cells

--- ticTacToeGame.sh
+++ ticTacToeGame.sh
@@ -1,19 +1,20 @@
-#!/bin/bash
+#!/bin/bash 
 
 echo ">>>>>>>>>>>>>>>>>>>> TIC TAC TOE <<<<<<<<<<<<<<<<<<"
 
 #CONSTANTS
 BOARD_BOX=9;
-DISTANCE=" "
+DISTANCE=" ";
+
 
 #assign variables
 declare -a designBoard
 
 #Start with resetting the board
 function resetPlayerBoard() {
-	for ((boardCellsRow=1;boardCellsRow<=$BOARD_BOX;boardCellsRow++))
+	for ((boardCellsRow=1;boardCellsRow<=3;boardCellsRow++))
 	do
-		for ((boardCellsColumn=1;boardCellsColumn<=$BOARD_BOX;boardCellsColumn++))
+		for ((boardCellsColumn=1;boardCellsColumn<=3;boardCellsColumn++))
 		do
 			designBoard[$boardCellsRow,$boardCellsColumn]=$DISTANCE
 		done
@@ -35,7 +36,9 @@ function selectionForLXandLO() {
 	playerToss
 	if [[ $tossCheck -eq 1 ]]
 	then
-		echo "Please choose game X OR game O"
+		echo "Please choose game sign to play"
+		echo "Press 1 for X"
+		echo "Press 2 for O"
 		read symbolSelection
 		playerChanceChoose
 	else
@@ -44,12 +47,36 @@ function selectionForLXandLO() {
 	fi 
 	
 	#Display the symbols of game
-	echo "Player gets $playerGameIcon"
-	echo "Computer gets $computerGameIcon"
+}
+
+function displayBoardBox() {
+	for ((boardCellsRow=1;boardCellsRow<=3;boardCellsRow++))
+	do
+		for ((boardCellsColumn=1;boardCellsColumn<=3;boardCellsColumn++))
+		do
+			echo " ${designBoard[$boardCellsRow,$boardCellsColumn]} "
+			columnDesign
+		done
+		rowDesign
+	done
+}
+
+function columnDesign() {
+	if [[ $boardCellsColumn -ne 3 ]]
+	then
+		echo " | "
+	fi
+}
+
+function rowDesign() {
+	if [[ $boardCellsRow -ne 3 ]]
+	then
+		echo " __ "
+	fi
 }
 
 function playerChanceChoose() {
-	if [[ $symbolSelection -eq 'X' ]]
+	if [[ $symbolSelection -eq 1 ]]
 	then
 		playerGameIcon=X;
 		computerGameIcon=O;
@@ -71,6 +98,10 @@ function computerChanceChoose() {
 }
 
 resetPlayerBoard
-playerToss
 selectionForLXandLO
+displayBoardBox
+
+echo "Player gets $playerGameIcon"
+echo "Computer gets $computerGameIcon"
+
 #End of Use Case 02
